6b2bf2a452850b1efbb9a4f5ea1e7f39dd99d662
MdUmar <risingsheikh@gmail.com>
2024-09-27 21:51:38 +0530

    Screen Optimization and Bug Fixes

--- PortfolioLOGIC.js
+++ PortfolioLOGIC.js
@@ -1,31 +1,34 @@
-var tab_links = document.getElementsByClassName("tab-links");
-var tabs = document.getElementsByClassName("tabs");
+
+// Funtcion to show the About me Grid for Skill, Experience & Education
 
 function openTab(tabId) {
+    let tab_links = document.getElementsByClassName("tab-links");
+    let tabs = document.getElementsByClassName("tabs");
+
     for (tab_link of tab_links) {
         tab_link.classList.remove("active-link");
     }
+
     for (tab of tabs) {
         tab.classList.remove("active-tab");
     }
-
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabId).classList.add("active-tab");
+    let tabID = document.getElementById(tabId);
+    event.target.classList.add("active-link");
+    tabID.classList.add("active-tab");
 }
 
-// optimized menu bar sidemenu
-var sidemeu = document.getElementById("sidemenu");
+// Burger menu open and close functions
 
 function openmenu() {
-    sidemeu.style.right = "0";
-    // document.getElementById("bar").style.display = "none";
+    sidemenu.style.right = "0";
+    bar.style.visibility = "hidden";
 }
 function closemenu() {
-    sidemeu.style.right = "-200px";
-    // document.getElementById("bar").style.display = "block";
+    sidemenu.style.right = "-200px";
+    bar.style.visibility = "visible";
 }
 
-// Sending mail from froms
+// Sending e-mail from form
 
 function emailSend() {
     var userName = document.getElementById('name').value;
@@ -50,3 +53,55 @@ function emailSend() {
     );
 }
 
+// fucntion to show and hide back to top button
+function backToTop() {
+    let topBtn = document.querySelector(".to-top");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            topBtn.classList.add("active");
+        } else {
+            topBtn.classList.remove("active");
+        }
+    });
+}
+
+function main() {
+
+    // Closing burger menu when a click is registered from outside of it
+    let sidemenu = document.getElementById("sidemenu");
+    let bar = document.getElementById("bar");
+    let cross = document.getElementById("cross");
+    document.addEventListener("click", (event) => {
+        if (!sidemenu.contains(event.target) && !cross.contains(event.target) && !bar.contains(event.target)) {
+            closemenu();
+        }
+    });
+
+    // Slider for my work projects slide through using Swiper API
+    var TrandingSlider = new Swiper('.tranding-slider', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        loop: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 30,
+            modifier: 2.5,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+
+    backToTop();
+
+}
+main();
